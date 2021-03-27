0988e99b6a664942e0cd1207dfad608139aafde0
GenkiOsawa <taiiku39@gmail.com>
2021-03-27 19:01:15 +0900

    [modify]いったんcommit

--- src/components/AnimesTable.vue
+++ src/components/AnimesTable.vue
@@ -1,8 +1,60 @@
 <template>
     <v-container>
+        <!-- 入力フォーム -->
+        <v-dialog v-model="dialog" max-width="500px">
+            <v-card>
+                <v-container>
+                    <h2 v-if="isCreate">アニメを追加する</h2>
+                    <h2 v-if="!isCreate">アニメを更新する</h2>
+                    <v-form ref="form" v-model="valid" lazy-validation>
+                        <!-- タイトル -->
+                        <v-text-field
+                            v-model="anime.title"
+                            :counter="20"
+                            label="タイトル"
+                            required
+                        ></v-text-field>
+
+                        <!-- 主人公 -->
+                        <v-text-field
+                            v-model="anime.hero"
+                            :rules="nameRules"
+                            :counter="20"
+                            label="名前"
+                            required
+                        ></v-text-field>
+                    </v-form>
+
+                    <!-- 追加ボタン -->
+                    <v-btn
+                        v-if="isCreate"
+                        :disabled="!valid"
+                        @click="createAnime"
+                    >
+                        追加
+                    </v-btn>
+
+                    <!-- 更新ボタン -->
+                    <v-btn 
+                        v-if="!isCreate"
+                        :disabled="!valid"
+                        @click="updateAnime"
+                    >
+                        更新
+                    </v-btn>
+                    <v-btn @click="clear">
+                        クリア
+                    </v-btn>
+                </v-container>
+            </v-card>
+        </v-dialog>
+
+
         <!-- ツールバー -->
         <v-toolbar flat color="grey lighten-2">
             <v-toolbar-title>アニメ一覧</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" dark class="mb-2" @click="showDialogNew">新規追加</v-btn>
         </v-toolbar>
 
         <!-- データテーブル -->
@@ -12,10 +64,25 @@
             no-data-text="アニメが登録されていません。"
             class="elevation-1"
         >
+            <v-progress-linear slot="progress" color="blue" indeterminate></v-progress-linear>
             <template slot="items" slot-scope="props">
                 <td>{{ props.item.id }}</td>
                 <td>{{ props.item.title }}</td>
                 <td>{{ props.item.hero }}</td>
+                <td class="justify-end layout px-0">
+                    <v-btn
+                        color="success"
+                        small
+                        outline
+                        flat
+                        @click="showDialogUpdate(props.item.id, props.item.name, props.item.gender)"
+                    >
+                        <v-icon small>
+                            edit
+                        </v-icon>
+                    </v-btn>
+                </td>
+                <!-- headerの中にボタン用の物がないから表示されない？？-->
             </template>
         </v-data-table>
     </v-container>
@@ -23,6 +90,7 @@
 
 <script>
     import { ALL_ANIMES } from "../constants/animes-query"
+    import { CREATE_ANIME, UPDATE_ANIME } from "../constants/animes-mutations"
     // import gql from 'graphql-tag'
 
     export default {
@@ -34,12 +102,92 @@
                 { text: 'ID', value: 'id' },
                 { text: 'タイトル', value: 'title' },
                 { text: '主人公', value: 'hero' },
+                { text: 'ボタン', value: 'action', sortable: false}
             ],
             // データテーブルの初期ソート
+            anime: {
+                title: '',
+                hero: '',
+            },
+            // バリデーション
+            valid: true,
+            nameRules: [
+                v => !!v || '名前は必須項目です',
+                v => (v && v.length <= 20) || '名前は20文字以内で入力してください'
+            ],
+            // ローディングの表示フラグ
+            progress: false,
+            // ダイアログの表示フラグ
+            dialog: false,
+            // 新規・更新フラグ
+            isCreate: true,
         }),
         apollo: {
             // すべての顧客情報の取得
             animes: ALL_ANIMES
+        },
+        methods: {
+            createAnime: function () {
+                if (this.$refs.form.validate()) {
+                    this.progress = true
+                    this.$apollo.mutate({
+                        mutation: CREATE_ANIME,
+                        variables: {
+                            title: this.anime.title,
+                            hero: this.anime.hero
+                        }
+                    }).then((data) => {
+                        // console.log(data.data.createAnime)
+                        this.animes = data.data.createAnime
+                        // this.$apollo.queries.animes.fetchMore({
+                        //     updateQuery: (previousResult, {fetchMoreResult}) => {
+                        //         console.log(fetchMoreResult.animes)
+                        //         return {
+                        //             animes: fetchMoreResult.animes
+                        //         }
+                        //     }
+                        // })
+                        this.dialog = false
+                        this.progress = false
+                    }).catch((error) => {
+                        console.error(error)
+                    })
+                }
+            },
+            updateAnime: function() {
+                this.progress = true
+                this.$apollo.mutate({
+                    mutation: UPDATE_ANIME,
+                    variables: {
+                        id: this.anime.id,
+                        title: this.anime.title,
+                        hero: this.anime.hero,
+                    }
+                }).then((data) => {
+                    this.animes = data.data.updateAnime.anime
+                    this.dialog = false
+                    this.progress = false
+                }).catch((error) => {
+                    console.error(error)
+                })
+            },
+            // フォームのクリア
+            clear: function() {
+                // this.$refs.form.reset()
+            },
+            // 新規追加ダイアログの表示
+            showDialogNew: function () {
+                this.clear() // 何をクリアしている???
+                this.isCreate = true
+                this.dialog = true
+            },
+            showDialogUpdate: function(id, title, hero) {
+                this.anime.id = id
+                this.anime.title = title
+                this.anime.hero = hero
+                this.isCreate = false
+                this.dialog = true
+            }
         }
     }
 </script>
---
+++ src/constants/animes-mutations.js
@@ -0,0 +1,26 @@
+import gql from 'graphql-tag'
+
+// animeの新規追加
+export const CREATE_ANIME = gql`
+    mutation($title: String, $hero: String) {
+        createAnime(title: $title, hero: $hero) {
+            id
+            title
+            hero
+        }
+    }
+`
+
+export const UPDATE_ANIME = gql`
+    mutation($animeId: String, $title: String, $hero: String) {
+        updateAnime(animeId: $animeId, title: $title, hero: $hero) {
+            success
+            message
+            anime {
+                id
+                title
+                hero
+            }
+        }
+    }
+`
