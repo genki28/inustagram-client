101e0c78623d78ac7a43bee3a657c6a38f9fa8b6
GenkiOsawa <taiiku39@gmail.com>
2021-04-03 16:36:28 +0900

    いったんコミット

---
+++ src/components/MessageTable.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        {{ messages }}
+    </div>
+</template>
+<script>
+import ALL_MESSAGE from '../constants/message-query'
+import CREATE_MESSAGE from '../constants/message-mutations'
+import SUBSCRIPTION_MESSAGE from '../constants/message-subscription'
+
+export default {
+    name: "MessageTable",
+    data: () => ({
+        // メッセージ配列
+        messages: [],
+        // message送信データの初期値
+        message: ''
+    }),
+    apollo: {
+        // すべてのメッセージ取得
+        messages: gql`
+            query messages {
+                id
+                sendUser
+                receiveUser
+                message
+            }
+        `,
+        subscribeToMore: {
+            Message: gql`
+                subscription {
+                    mutation
+
+                    data {
+                        id
+                        sendUser
+                        receiveUser
+                        message
+                    }
+                }
+            `
+        }
+    },
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,23 +2,59 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import { ApolloClient } from 'apollo-client';
+
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import VueApollo from 'vue-apollo';
+import { split } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
 
 Vue.config.productionTip = false
 Vue.use(VueApollo)
 
 // HTTP connection to the API
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql'
+  uri: 'http://localhost:4000/graphql',
+  // 将来的に入れる認証
+  // headers: {
+  //   authorization: `Bearer xxxxx`
+  // }
+})
+
+// subscriptionで使われるwebsocket通信のエンドポイントを指定
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:4000/graphql",
+  options: {
+    reconnect: true,
+    // // 将来的に入れる認証
+    // headers: {
+    //   authorization: `Bearer xxxxx`
+    // }
+  }
 })
 
 const cache = new InMemoryCache()
 
+// GraphQLのエンドポイントの向き先をクエリの種別で分離する
+// ここに参考あります→https://www.apollographql.com/docs/link/composition/#directional-composition
+const link = split(
+  ({ query }) => {
+    // クエリから種別を取得
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === "OperationDefinition" && 
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+)
+
 const apolloClient = new ApolloClient({
-  link: httpLink,
+  link,
   cache,
+  connectToDevTools: true
 })
 
 const apolloProvider = new VueApollo({
